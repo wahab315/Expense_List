cecdb00c3ddf09d87a87fb7d609d870eb93e497f
Abdul Wahab <102620981+wahab315@users.noreply.github.com>
2023-03-01 01:28:13 +0500

    new code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 import "./App.css";
+import { ExpenseItem } from "./components";
 
 function App() {
-  return <>
+  const expenses = [
+    {
+      id: "1",
+      title: "First",
+      amount: 94.32,
+      date: new Date(2020, 2, 9),
+    },
+    {
+      id: "2",
+      title: "Second ",
+      amount: 21.12,
+      date: new Date(2023, 11, 1),
+    },
+    {
+      id: "3",
+      title: "third",
+      amount: 34.22,
+      date: new Date(2021, 12, 6),
+    },
+  ];
 
-
-
-  
-  
-  </>;
+  return (
+    <>
+      <div className="background">
+        <div className="background-color">
+          <ExpenseItem
+            title={expenses[0].title}
+            amount={expenses[0].amount}
+            date={expenses[0].date}
+          />
+          <ExpenseItem
+            title={expenses[1].title}
+            amount={expenses[1].amount}
+            date={expenses[1].date}
+          />
+          <ExpenseItem
+            title={expenses[2].title}
+            amount={expenses[2].amount}
+            date={expenses[2].date}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export { default as ExpenseItem } from "./expenseItem/ExpenseItem";
