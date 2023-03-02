7ceec2fae54989b22f7e87d753349087f35b114a
Abdul Wahab <102620981+wahab315@users.noreply.github.com>
2023-03-03 04:23:47 +0500

    new code

--- src/App.js
+++ src/App.js
@@ -32,21 +32,6 @@ function App() {
       <div className="background">
         <div className="background-color">
           <h2>Let's get Started !</h2>
-          {/* <ExpenseItem
-            title={expenses[0].title}
-            amount={expenses[0].amount}
-            date={expenses[0].date}
-          />
-          <ExpenseItem
-            title={expenses[1].title}
-            amount={expenses[1].amount}
-            date={expenses[1].date}
-          />
-          <ExpenseItem
-            title={expenses[2].title}
-            amount={expenses[2].amount}
-            date={expenses[2].date}
-          /> */}
           <NewExpense onAddExpense={addExpenseHandler} />
           <Expenses items={expenses} />
         </div>
